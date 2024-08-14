5fdf17ebafd3c2bf055439f91c8b5b78b0aff26f
Harshit Singla <harshitsingla1761@gmail.com>
2024-08-14 14:05:58 +0530

    Consoling Again to debug

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -4,6 +4,8 @@ export const verifyToken = (req, res, next) => {
 
     const token = req.cookies.token;
 
+    console.log("Cookies", req.cookies);
+
     console.log("Token", token);
 
     if (!token) {
